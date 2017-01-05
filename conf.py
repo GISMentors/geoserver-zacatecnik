e442152eac095027e4e78e82ebd3f0b454256560
Martin Landa <landa.martin@gmail.com>
2017-01-05 15:15:12 +0100

    fix version and copyright (same with other materials)

--- conf.py
+++ conf.py
@@ -10,16 +10,16 @@ from utils import get_month_year, get_year
 
 # General information about the project.
 project = u'Školení GeoServer pro začátečníky'
-copyright = u'2016-%d, Jan Růžička  (GISMentors.eu)' % get_year()
+copyright = u'2016-%d GISMentors' % get_year()
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '0.9'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
-release = '%sbeta' % version
+release = '%salfa' % version
 
 # -- Options for HTML output ----------------------------------------------
 
